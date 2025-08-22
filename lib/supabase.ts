a279b1ffb892481c60d0ea1550dfcf4ce9002f21
olivermacdonald07-boop <olivermacdonald07@gmail.com>
2025-08-22 15:05:22 +0000

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL=https://fcpgyoxhrfufhegcyqtr.supabase.co
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY=sb_publishable_kDiu1yRLwLPUL5Wg-lnowQ_0B8FmMFy
 )
 
 export default supabase;
