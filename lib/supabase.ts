75baec059f0dcdd9a37eeb0f3c37565a56a79b85
olivermacdonald07-boop <olivermacdonald07@gmail.com>
2025-08-27 15:35:28 +0000

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,3 +4,5 @@ const supabaseUrl = "https://fcpgyoxhrfufhegcyqtr.supabase.co";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(https://fcpgyoxhrfufhegcyqtr.supabase.co, supabaseAnonKey);
+
+)
